c6e5a9e7839c475eba022e6f11010034c84463c1
Himanshu Singh <himanshurpsingh@gmail.com>
2023-08-08 10:18:45 +0530

    project created

---
+++ citisers/android/app/src/main/kotlin/com/example/citisers/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.citisers
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ citisers/lib/utils/theme_provider.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ThemeModel extends ChangeNotifier {
+  bool _isDarkMode = false;
+
+  bool get isDarkMode => _isDarkMode;
+
+  void toggleTheme() {
+    _isDarkMode = !_isDarkMode;
+    notifyListeners();
+  }
+}
